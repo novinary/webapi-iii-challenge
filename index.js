3edbda4d3b7f8ecf2e82194f0e54fa8804bec8fe
novinary <novina.dev@gmail.com>
2019-03-28 13:37:58 +0000

    deploy changes

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const server = require('./server.js');
 // heroku will have the PORT environment variable set
 const port = process.env.PORT || 4444;
 
-// we can now use that port, if set up by heroku or read from .env or 5000 as a default if not set
+// we can now use that port, if set up by heroku or read from .env or 4444 as a default if not set
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
--- server.js
+++ server.js
@@ -10,4 +10,10 @@ server.use(express.json());
 server.use("/api/users", usersRouter);
 server.use("/api/posts", postsRouter);
 
+server.get('/', (req, res) => {
+  
+            res.status(200).json({'test':'just a test'});
+
+});
+
 module.exports = server;
