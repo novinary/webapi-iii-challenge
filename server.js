139c1773e12818b817e6367b4cd5631a3236e730
novinary <novina.dev@gmail.com>
2019-03-28 13:53:21 +0000

    Using helmet

--- server.js
+++ server.js
@@ -1,18 +1,20 @@
 const express = require("express");
-
 const server = express();
+const helmet = require("helmet");
+
 const usersRouter = require("./usersRouter.js");
 const postsRouter = require("./postsRouter.js");
 
 server.use(express.json());
+server.use(helmet());
 
 // routing
 server.use("/api/users", usersRouter);
 server.use("/api/posts", postsRouter);
 
 server.get('/', (req, res) => {
-  
-            res.status(200).json({'test':'just a test'});
+
+    res.status(200).json({ 'test': 'just a test' });
 
 });
 
