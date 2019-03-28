0e2d6b83a3611b7dfd825d62d2c44fe5b963eca2
novinary <novina.dev@gmail.com>
2019-03-28 12:45:55 +0000

    port change

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const server = require('./server.js');
 
 // we'll read the port from the server environment if it is there
 // heroku will have the PORT environment variable set
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4444;
 
 // we can now use that port, if set up by heroku or read from .env or 5000 as a default if not set
 server.listen(port, () => {
